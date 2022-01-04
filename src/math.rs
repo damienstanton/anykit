0b9758499c60176e68a62cd33228b88a29206a8b
Damien Stanton <damien.stanton@pwc.com>
2022-01-04 10:38:03 -0500

    Add doc and unit tests to the fake internal crate

--- src/math.rs
+++ src/math.rs
@@ -1,4 +1,18 @@
 //! A boilerplate internal module for a private API
 
 /// Returns the result of adding two integers.
+/// ```
+/// use anykit::math;
+/// 
+/// assert_eq!(math::add(1,0), 1);
+/// ```
 pub fn add(a: i32, b: i32) -> i32 { a + b }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn smoke() {
+        assert_eq!(add(1, 1), 2);
+    }
+}
