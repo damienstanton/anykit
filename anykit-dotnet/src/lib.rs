60abb62b582c347f8945afcdee9432c72b49c1a5
Damien Stanton <damien.stanton@gmail.com>
2022-01-20 12:07:31 -0500

    Add a small stub for dotnet

--- anykit-dotnet/src/lib.rs
+++ anykit-dotnet/src/lib.rs
@@ -1,5 +1,17 @@
+use anykit::math;
+use std::ffi::CString;
+
+#[no_mangle]
+pub extern "C" fn sum_as_string(a: i32, b: i32) -> CString {
+    // TODO: correct this impl
+    // math::add(a, b)
+    todo!()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
